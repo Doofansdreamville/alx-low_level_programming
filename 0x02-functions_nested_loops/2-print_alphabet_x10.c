a2d4cb2a2e576e2a7bd0ec5cb63d228b1facaf8d
“Wilma <wilmadoofan.tar@gmail.com>
2022-09-14 04:02:31 -0700

    point

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - print alphabet in lowercase ten times
-* Return : 0
-*/
+ * print_alphabet_x10 - print alphabet in lowercase ten times
+ * Return : 0
+ */
 void print_alphabet_x10(void)
 {
 	int i = 0;
